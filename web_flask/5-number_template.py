65f2212974b81785ac492b3361fbed517dbe766b
salahmalih <C130084552@taalim.ma>
2024-06-02 06:37:14 -0700

    all

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+""" Script that starts a Flask web application """
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def hello_hbnb():
+    """print web"""
+    return 'Hello HBNB!'
+
+@app.route('/hbnb')
+def hbnb():
+    return 'HBNB'
+
+@app.route('/c/<text>')
+def c_is_fun(text):
+    """print C followed by the value of the text variable"""
+    return 'C {}'.format(text.replace('_', ' '))
+
+@app.route('/python')
+@app.route('/python/<text>')
+def python_is_cool(text='is cool'):
+    return 'python {}'.format(text.replace('_', ' '))
+
+@app.route('/number/<int:n>')
+def is_number(n):
+    return '{} is number'.format(n)
+
+@app.route('/number_template/<int:n>')
+def number_template(n):
+    return render_template("5-number.html", n=n)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
